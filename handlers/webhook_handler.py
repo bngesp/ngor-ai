bf52cc61ad2eb6aa40aea3cd609e6850c6ca5690
bassirou ngom <bassiroungom@esp.sn>
2025-03-07 21:47:02 +0000

    analyse ligne par ligne

--- handlers/webhook_handler.py
+++ handlers/webhook_handler.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 
 from core.review_manager import MergeRequest
 from core.review_manager import process_merge_request
+import logging
 
 app = FastAPI()
 
@@ -13,14 +14,19 @@ async def webhook(request: Request):
         return {"status": "ignored"}
 
     mr_data = payload['object_attributes']
-    mr = MergeRequest(
-        iid=mr_data['iid'],
-        source_branch=mr_data['source_branch'],
-        base_sha=mr_data['target']['commit']['sha'],
-        start_sha=mr_data['source']['commit']['sha'],
-        head_sha=mr_data['last_commit']['id']
-    )
+    logging.info(f"Webhook reçu pour la MR: {mr_data['iid']}")
+    try:
+        logging.info(f"Traitement de la MR: {mr_data['iid']}")
+        mr = MergeRequest(
+            iid=mr_data['iid'],
+            source_branch=mr_data['source_branch'],
+            base_sha=mr_data['target']['commit']['sha'],
+            start_sha=mr_data['source']['commit']['sha'],
+            head_sha=mr_data['last_commit']['id']
+        )
 
-    process_merge_request(mr)
+        process_merge_request(mr)
+    except Exception as e:
+        logging.error(f"Erreur lors du traitement de la MR: {e}")
 
     return {"status": "processed"}
