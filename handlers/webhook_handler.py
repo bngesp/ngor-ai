a21ff09791d3e50f826db91d2fb584b1aed09dc0
bassirou ngom <bassiroungom@esp.sn>
2025-03-08 19:43:47 +0000

    analyse ligne par ligne

--- handlers/webhook_handler.py
+++ handlers/webhook_handler.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 
+from core.gitlab_client import get_changed_files
 from core.review_manager import MergeRequest
 from core.review_manager import process_merge_request
 import logging
@@ -18,13 +19,14 @@ async def webhook(request: Request):
     logging.info(f"Webhook reçu pour la MR: {mr_data['iid']}")
     try:
         logging.info(f"Traitement de la MR: {mr_data['iid']}")
-        mr = MergeRequest(
-            iid=mr_data['iid'],
-            source_branch=mr_data['source_branch'],
-            base_sha=mr_data['target']['commit']['sha'],
-            start_sha=mr_data['source']['commit']['sha'],
-            head_sha=mr_data['last_commit']['id']
-        )
+        changed_files = get_changed_files(mr_data['iid'])
+        # mr = MergeRequest(
+        #     iid=mr_data['iid'],
+        #     source_branch=mr_data['source_branch'],
+        #     base_sha=mr_data['diff_refs']['base_sha'],
+        #     start_sha=mr_data['diff_refs']['start_sha'],
+        #     head_sha=mr_data['diff_refs']['head_sha']
+        # )
 
         process_merge_request(mr)
     except Exception as e:
